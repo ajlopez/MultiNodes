9ed8b213273e9cd423129bf16a3861afdf624f01
ajlopez <webmaster@ajlopez.com>
2013-01-27 19:18:26 -0300

    Refactor callback in connect, listen, with test

--- test/network.js
+++ test/network.js
@@ -2,7 +2,7 @@
 var mnode = require('..');
 
 exports['Send Message to Connected Node'] = function (test) {
-    test.expect(4);
+    test.expect(10);
     
     var nmsg = 0;
 
@@ -14,11 +14,19 @@ exports['Send Message to Connected Node'] = function (test) {
     node.registerService('service1', new Service(test, 1, result, done));
     node2.registerService('service2', new Service(test, 2, result, done));
     
-    node.listen(3000, function () {
+    node.listen(3000, function (client, msg) {
+        test.equal(msg.id, node2.getDescription().id);
+        test.ok(msg.services);
+        test.ok(msg.services.service2);
+
         node.process({ service: 'service2', message: 2 });
     });
     
-    node2.connect(3000, function () {
+    node2.connect(3000, function (server, msg) {
+        test.equal(msg.id, node.getDescription().id);
+        test.ok(msg.services);
+        test.ok(msg.services.service1);
+
         node2.process({ service: 'service1', message: 1 });
     });
         
