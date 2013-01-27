8c111e97b1726158f56c2579346035385643ac54
ajlopez <webmaster@ajlopez.com>
2013-01-27 17:18:29 -0300

    getNodes with service name and filter

--- test/connect.js
+++ test/connect.js
@@ -56,13 +56,42 @@ exports['Get Nodes with Service'] = function (test) {
     var node2 = mnode.createNode();
     var node3 = mnode.createNode();
     
-    node2.registerService('service1', {});
-    node3.registerService('service3', {});
+    node2.registerService('service1');
+    node3.registerService('service2');
     
     node2.connect(node);
     node3.connect(node);
     
     var nodes = node.getNodes('service1');
+    
+    test.ok(nodes);
+    test.equal(Object.keys(nodes).length, 1);
+    
+    var desc = node2.getDescription();
+    
+    var nodeinfo = nodes[desc.id];
+    
+    test.ok(nodeinfo);
+    test.equal(nodeinfo.id, desc.id);
+    test.ok(nodeinfo.info);
+    test.deepEqual(nodeinfo.info, desc);
+    
+    test.done();
+};
+
+exports['Get Nodes with Service and Filter'] = function (test) {
+    var nmsg = 0;
+    var node = mnode.createNode();
+    var node2 = mnode.createNode();
+    var node3 = mnode.createNode();
+    
+    node2.registerService('service1', { }, { counter: 10 });
+    node3.registerService('service1', { }, { counter: 20 });
+    
+    node2.connect(node);
+    node3.connect(node);
+    
+    var nodes = node.getNodes('service1', function (desc) { return desc.counter == 10; });
     
     test.ok(nodes);
     test.equal(Object.keys(nodes).length, 1);
