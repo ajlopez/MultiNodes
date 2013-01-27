d91f13e8243f91217cbfffca7d5decf6cd7aa506
ajlopez <webmaster@ajlopez.com>
2013-01-27 17:05:22 -0300

    getNodes with service name

--- test/connect.js
+++ test/connect.js
@@ -45,6 +45,36 @@ exports['Connect and Get Nodes'] = function (test) {
     
     test.ok(nodeinfo);
     test.equal(nodeinfo.id, desc.id);
+    test.ok(nodeinfo.info);
+    
+    test.done();
+};
+
+exports['Get Nodes with Service'] = function (test) {
+    var nmsg = 0;
+    var node = mnode.createNode();
+    var node2 = mnode.createNode();
+    var node3 = mnode.createNode();
+    
+    node2.registerService('service1', {});
+    node3.registerService('service3', {});
+    
+    node2.connect(node);
+    node3.connect(node);
+    
+    var nodes = node.getNodes('service1');
+    
+    test.ok(nodes);
+    test.equal(Object.keys(nodes).length, 1);
+    
+    var desc = node2.getDescription();
+    
+    var nodeinfo = nodes[desc.id];
+    
+    test.ok(nodeinfo);
+    test.equal(nodeinfo.id, desc.id);
+    test.ok(nodeinfo.info);
+    test.deepEqual(nodeinfo.info, desc);
     
     test.done();
 };
